c7e258b8316fbe07a8a465c682fc3a71fd4bda20
akk1921 <aikikarius@gmail.com>
2022-04-29 18:47:22 +0800

    1st push
    首次推送

---
+++ tasomachi/tiles/data.js
@@ -0,0 +1,58 @@
+var itemData = {
+	"shioItems":[
+		{"locationCategory":"shiokaze-earth-src","id":1,"name":"潮风地能1","h":146.5,"s":124,"content":"传说中，它是油纸伞放置100年后变化而成的"},
+		{"locationCategory":"shiokaze-earth-src","id":2,"name":"潮风地能2","h":42.75,"s":29.875,"content":"传说中，它是油纸伞放置101年后变化而成的"},
+		{"locationCategory":"shiokaze-earth-src","id":3,"name":"潮风地能3","h":237.75,"s":119,"content":"传说中，它是油纸伞放置102年后变化而成的"},
+		{"locationCategory":"shiokaze-costume","id":4,"name":"潮风衣服1","h":55.5,"s":156.625,"content":"传说中，它是油纸伞放置103年后变化而成的"},
+		{"locationCategory":"shiokaze-costume","id":5,"name":"潮风衣服2","h":135.75,"s":14.875,"content":"传说中，它是油纸伞放置104年后变化而成的"},
+		{"locationCategory":"shiokaze-costume","id":6,"name":"潮风衣服3","h":201.125,"s":29.5,"content":"传说中，它是油纸伞放置105年后变化而成的"},
+		{"locationCategory":"shiokaze-fur","id":7,"name":"潮风家具1","h":21.375,"s":50.875,"content":"传说中，它是油纸伞放置106年后变化而成的"},
+		{"locationCategory":"shiokaze-fur","id":8,"name":"潮风家具2","h":151.5,"s":150.625,"content":"传说中，它是油纸伞放置107年后变化而成的"},
+		{"locationCategory":"shiokaze-fur","id":9,"name":"潮风家具3","h":83.625,"s":93.5,"content":"传说中，它是油纸伞放置108年后变化而成的"},
+		{"locationCategory":"shiokaze-note","id":10,"name":"潮风笔记1","h":93,"s":103,"content":"传说中，它是油纸伞放置109年后变化而成的"},
+		{"locationCategory":"shiokaze-note","id":11,"name":"潮风笔记2","h":103,"s":113,"content":"传说中，它是油纸伞放置110年后变化而成的"},
+		{"locationCategory":"shiokaze-note","id":12,"name":"潮风笔记3","h":113,"s":123,"content":"传说中，它是油纸伞放置111年后变化而成的"},
+		{"locationCategory":"shiokaze-corrupt","id":13,"name":"潮风损坏地能1","h":22,"s":25,"content":"传说中，它是油纸伞放置112年后变化而成的"},
+		{"locationCategory":"shiokaze-corrupt","id":14,"name":"潮风损坏地能2","h":12,"s":5,"content":"传说中，它是油纸伞放置113年后变化而成的"},
+		{"locationCategory":"shiokaze-corrupt","id":15,"name":"潮风损坏地能3","h":99,"s":200,"content":"传说中，它是油纸伞放置114年后变化而成的"},
+		
+		],
+	"futaItems":[
+		{"locationCategory":"futago-earth-src","id":10,"name":"双子地能1","h":26.5,"s":60.406,"content":"有木灵寄宿的树木外观和普通的树木无异"},
+		{"locationCategory":"futago-earth-src","id":11,"name":"双子地能2","h":217.375,"s":120.125,"content":"有木灵寄宿的树木外观和普通的树木无异"},
+		{"locationCategory":"futago-earth-src","id":12,"name":"双子地能3","h":9.125,"s":116.125,"content":"有木灵寄宿的树木外观和普通的树木无异"},
+		{"locationCategory":"futago-costume","id":13,"name":"双子衣服1","h":101.625,"s":151.375,"content":"有木灵寄宿的树木外观和普通的树木无异"},
+		{"locationCategory":"futago-costume","id":14,"name":"双子衣服2","h":170.375,"s":104.625,"content":"有木灵寄宿的树木外观和普通的树木无异"},
+		{"locationCategory":"futago-costume","id":15,"name":"双子衣服3","h":54.125,"s":98.375,"content":"有木灵寄宿的树木外观和普通的树木无异"},
+		{"locationCategory":"futago-fur","id":16,"name":"双子家具1","h":195.375,"s":155,"content":"有木灵寄宿的树木外观和普通的树木无异"},
+		{"locationCategory":"futago-fur","id":17,"name":"双子家具2","h":202.875,"s":86.5,"content":"有木灵寄宿的树木外观和普通的树木无异"},
+		{"locationCategory":"futago-fur","id":18,"name":"双子家具3","h":128,"s":75.25,"content":"有木灵寄宿的树木外观和普通的树木无异"},
+		{"locationCategory":"futago-note","id":10,"name":"双子笔记1","h":93,"s":103,"content":"有木灵寄宿的树木外观和普通的树木无异"},
+		{"locationCategory":"futago-note","id":11,"name":"双子笔记2","h":103,"s":113,"content":"有木灵寄宿的树木外观和普通的树木无异"},
+		{"locationCategory":"futago-note","id":12,"name":"双子笔记3","h":113,"s":123,"content":"有木灵寄宿的树木外观和普通的树木无异"},
+		{"locationCategory":"futago-corrupt","id":13,"name":"双子损坏地能1","h":22,"s":25,"content":"有木灵寄宿的树木外观和普通的树木无异"},
+		{"locationCategory":"futago-corrupt","id":14,"name":"双子损坏地能2","h":12,"s":5,"content":"有木灵寄宿的树木外观和普通的树木无异"},
+		{"locationCategory":"futago-corrupt","id":15,"name":"双子损坏地能3","h":99,"s":200,"content":"有木灵寄宿的树木外观和普通的树木无异"},
+		
+		],
+
+	"genItems":[
+		{"locationCategory":"gengetsu-earth-src","id":19,"name":"弦月地能1","h":167.375,"s":43.25,"content":"Nurikabe（ぬりかべ）来自‘化物之绘’(约1700年）"},
+		{"locationCategory":"gengetsu-earth-src","id":20,"name":"弦月地能2","h":171.625,"s":93.5,"content":"Nurikabe（ぬりかべ）来自‘化物之绘’(约1701年）"},
+		{"locationCategory":"gengetsu-earth-src","id":21,"name":"弦月地能3","h":206.75,"s":148.25,"content":"Nurikabe（ぬりかべ）来自‘化物之绘’(约1702年）"},
+		{"locationCategory":"gengetsu-costume","id":22,"name":"弦月衣服1","h":97.25,"s":61.25,"content":"Nurikabe（ぬりかべ）来自‘化物之绘’(约1703年）"},
+		{"locationCategory":"gengetsu-costume","id":23,"name":"弦月衣服2","h":138.375,"s":109.875,"content":"Nurikabe（ぬりかべ）来自‘化物之绘’(约1704年）"},
+		{"locationCategory":"gengetsu-costume","id":24,"name":"弦月衣服3","h":209,"s":30.125,"content":"Nurikabe（ぬりかべ）来自‘化物之绘’(约1705年）"},
+		{"locationCategory":"gengetsu-fur","id":25,"name":"弦月家具1","h":123.375,"s":110.875,"content":"Nurikabe（ぬりかべ）来自‘化物之绘’(约1706年）"},
+		{"locationCategory":"gengetsu-fur","id":27,"name":"弦月家具2","h":28,"s":133.25,"content":"一般对祂的表述为红脸垂髪的五，六岁小孩子"},
+		{"locationCategory":"gengetsu-fur","id":26,"name":"弦月家具3","h":88,"s":50,"content":"一般对祂的表述为红脸垂髪的五，六岁小孩子"},
+		{"locationCategory":"gengetsu-note","id":10,"name":"弦月笔记1","h":93,"s":103,"content":"有木灵寄宿的树木外观和普通的树木无异"},
+		{"locationCategory":"gengetsu-note","id":11,"name":"弦月笔记2","h":103,"s":113,"content":"有木灵寄宿的树木外观和普通的树木无异"},
+		{"locationCategory":"gengetsu-note","id":12,"name":"弦月笔记3","h":113,"s":123,"content":"有木灵寄宿的树木外观和普通的树木无异"},
+		{"locationCategory":"gengetsu-corrupt","id":13,"name":"弦月损坏地能1","h":22,"s":25,"content":"有木灵寄宿的树木外观和普通的树木无异"},
+		{"locationCategory":"gengetsu-corrupt","id":14,"name":"弦月损坏地能2","h":12,"s":5,"content":"有木灵寄宿的树木外观和普通的树木无异"},
+		{"locationCategory":"gengetsu-corrupt","id":15,"name":"弦月损坏地能3","h":99,"s":200,"content":"有木灵寄宿的树木外观和普通的树木无异"},
+		
+		]
+
+}
---
+++ tasomachi/tiles/twilight.js
@@ -0,0 +1,431 @@
+//设定边界范围
+var   SW = L.latLng(-305,-120),
+       NE = L.latLng(135, 487),
+       bounds = L.latLngBounds(SW, NE);
+
+
+
+//定义地图
+var map = L.map('lmap', {
+    crs: L.CRS.Simple,
+    minZoom: 1,
+    maxZoom:3,
+    maxNativeZoom: 5, 
+    maxBounds: bounds, //最大边界
+    zoom:3,
+    tap: false, 
+    doubleClickZoom: true,
+    center:new L.latLng(-27,51)});
+
+//读取瓦片
+var opt = {tileSize: L.point(256, 256)};
+shiokaze=L.tileLayer('shiokaze/tiles/{z}/{x}/{y}.png', opt);
+futago=L.tileLayer('futago/tiles/{z}/{x}/{y}.png', opt);
+gengetsu=L.tileLayer('gengetsu/tiles/{z}/{x}/{y}.png', opt);
+ 
+shiokaze.addTo(map);
+
+map.setView([-130,187], 2);
+
+//设定缩放按钮属性
+map.zoomControl.setPosition('bottomleft');
+
+
+var customPopup =
+        {
+            'maxWidth': '300',
+            'className': 'customPopup'            
+        }
+var contentPopup = "" 
+
+//---------------pre search-------------------
+var shioKensaku =new L.layerGroup(); //定义搜索组
+      futaKensaku =new L.layerGroup(); 
+      genKensaku =new L.layerGroup(); 
+
+//map.addLayer(shioKensaku); //将搜索组添加到地图
+//map.addLayer(genKensaku);
+//map.addLayer(futaKensaku);
+
+
+//-----------------------create markers-----------------------------
+    //marker icon setting
+    var MarkerIconSize = 40,
+        MarkerIconCenter = MarkerIconSize / 2 -3.5,
+        MarkerPopPosition = -MarkerIconSize - 2,
+
+        shiokazeMKe=[], 
+        shiokazeMKf=[],
+        shiokazeMKc=[],
+        shiokazeMKn=[],
+        shiokazeMKco=[],
+        futagoMKe=[], 
+        futagoMKf=[],
+        futagoMKc=[],
+        futagoMKn=[],
+        futagoMKco=[],
+        gengetsuMKe=[], 
+        gengetsuMKf=[],
+        gengetsuMKn=[], 
+        gengetsuMKco=[],
+        gengetsuMKc=[];
+        
+//--------------------shiomk创建------------------------
+    for (i = 0; i < itemData.shioItems.length; i++) {
+//往各marker组内填充数据
+
+let j = itemData.shioItems[i],
+            icon = L.icon({
+                iconUrl: 'tiles/Sprite/icon/' + j.locationCategory + '.png',
+                iconSize: [40, ],
+                iconAnchor: [MarkerIconCenter, MarkerIconSize],
+                shadowSize: [0, 0],
+                popupAnchor: [0, MarkerPopPosition],
+            });
+                
+        let  mkShio =  new L.marker(new L.latLng([-j.h, j.s]),{icon: icon,title:j.name}).bindPopup(
+            "<h3>" + j.name + "</h3><br>" + j.content, customPopup);
+       shioKensaku.addLayer(mkShio);
+
+//marker图层分组
+           switch(j.locationCategory){
+                case "shiokaze-earth-src":
+                    shiokazeMKe.push(mkShio);
+                    break;
+                case "shiokaze-fur":
+                    shiokazeMKf.push(mkShio);
+                    break;
+                case "shiokaze-costume":
+                    shiokazeMKc.push(mkShio);
+                    break;
+                case "shiokaze-note":
+                        shiokazeMKn.push(mkShio);
+                        break;
+                case "shiokaze-corrupt":
+                        shiokazeMKco.push(mkShio);
+                        break;
+            }
+    }
+   
+    //---------------------FUTA MKS---------------
+
+    for (i = 0; i < itemData.futaItems.length; i++) {
+        //往各marker组内填充数据
+        
+        let j = itemData.futaItems[i],
+                    icon = L.icon({
+                        iconUrl: 'tiles/Sprite/icon/' + j.locationCategory + '.png',
+                        iconSize: [40, ],
+                        iconAnchor: [MarkerIconCenter, MarkerIconSize],
+                        shadowSize: [0, 0],
+                        popupAnchor: [0, MarkerPopPosition],
+                    });
+                        
+                let  mkFuta =  new L.marker(new L.latLng([-j.h, j.s]),{icon: icon,title:j.name}).bindPopup(
+                    "<h3>" + j.name + "</h3><br>" + j.content, customPopup);
+               futaKensaku.addLayer(mkFuta);
+        
+                   switch(j.locationCategory){
+                    
+                            case "futago-earth-src":
+                                futagoMKe.push(mkFuta);
+                                break;
+                            case "futago-fur":
+                                futagoMKf.push(mkFuta);
+                                break;
+                            case "futago-costume":
+                                futagoMKc.push(mkFuta);
+                                break;
+                             case "futago-note":
+                        futagoMKn.push(mkFuta);
+                        break;
+                        case "futago-corrupt":
+                        futagoMKco.push(mkFuta);
+                        break;
+                                      
+                    }
+            }
+
+//----------------------------gen MKS---------------------
+
+for (i = 0; i < itemData.genItems.length; i++) {
+    //往各marker组内填充数据
+    
+    let j = itemData.genItems[i],
+                icon = L.icon({
+                    iconUrl: 'tiles/Sprite/icon/' + j.locationCategory + '.png',
+                    iconSize: [40, ],
+                    iconAnchor: [MarkerIconCenter, MarkerIconSize],
+                    shadowSize: [0, 0],
+                    popupAnchor: [0, MarkerPopPosition],
+                });
+                    
+            let  mkGen =  new L.marker(new L.latLng([-j.h, j.s]),{icon: icon,title:j.name}).bindPopup(
+                "<h3>" + j.name + "</h3><br>" + j.content, customPopup);
+           genKensaku.addLayer(mkGen);
+    
+               switch(j.locationCategory){                  
+                            case "gengetsu-earth-src":
+                                gengetsuMKe.push(mkGen);
+                                break;
+                            case "gengetsu-fur":
+                                gengetsuMKf.push(mkGen);
+                                break;
+                            case "gengetsu-costume":
+                                gengetsuMKc.push(mkGen);
+                                break;
+                            case "gengetsu-note":
+                                    gengetsuMKn.push(mkGen);
+                                    break;
+                            case "gengetsu-corrupt":
+                                    gengetsuMKco.push(mkGen);
+                                    break;
+                    
+                }
+        }
+
+
+    //将marker分组
+    var    she=L.layerGroup(shiokazeMKe),
+            shc=L.layerGroup(shiokazeMKc),
+            shf=L.layerGroup(shiokazeMKf),
+            shn=L.layerGroup(shiokazeMKn),
+            sho=L.layerGroup(shiokazeMKco),
+         //   sh=L.layerGroup(shiokazeMKc.concat(shiokazeMKe,shiokazeMKf)),
+            
+            gee=L.layerGroup(gengetsuMKe),
+            gec=L.layerGroup(gengetsuMKc),
+            gef=L.layerGroup(gengetsuMKf),
+            gen=L.layerGroup(gengetsuMKn),
+            geo=L.layerGroup(gengetsuMKco),
+         //   ge=L.layerGroup(gengetsuMKc.concat(gengetsuMKe,gengetsuMKf)),
+
+            fue=L.layerGroup(futagoMKe),
+            fuc=L.layerGroup(futagoMKc),
+            fun=L.layerGroup(futagoMKn),
+            fuo=L.layerGroup(futagoMKco),
+            fuf=L.layerGroup(futagoMKf);
+
+       // all=L.layerGroup(shiokazeMKc.concat(shiokazeMKe,shiokazeMKf,gengetsuMKc,gengetsuMKe,gengetsuMKf,futagoMKc,futagoMKe,futagoMKf));
+
+         //   
+
+           
+//-----------------------deploy markers-----------------------------
+          // all.addTo(map);
+  //      const jr=[she,shc,shf,gee,gec,gef,fue,fuc,fuf,sh,ge,sh];
+  //      for (u = 0; u < jr.length; u++){
+  //          map.addLayer(jr[u])};
+        
+   //         all.removeFrom(map);
+
+//------------------------------------------搜索
+//SHIO搜索控件
+var shioKensakuCtrl = new L.Control.Search({
+    position:'topleft',		
+    container:'kensaku',
+    textPlaceholder: '请输入物品名称后按回车搜索1....',
+    textErr: '未搜索到相关内容',
+    layer: shioKensaku,
+    initial: false, 
+    zoom: 4, 
+    delayType:40, 
+    collapsed:false, //控制搜索框默认是否折叠
+    marker: false})
+
+map.addControl(shioKensakuCtrl);
+shioKensakuCtrl.on('search:locationfound', function(e) {    
+	e.layer.openPopup();
+});     //搜索完成后移动到相应位置
+
+//futa搜索控件
+var futaKensakuCtrl = new L.Control.Search({
+    position:'topleft',		
+    container:'kensaku',
+    textPlaceholder: '请输入物品名称后按回车搜索2....',
+    textErr: '未搜索到相关内容',
+    layer: futaKensaku,
+    initial: false, 
+    zoom: 4, 
+    delayType:40, 
+    collapsed:false, 
+    marker: false})
+//map.addControl(futaKensakuCtrl);
+futaKensakuCtrl.on('search:locationfound', function(e) {    
+	e.layer.openPopup();
+});     
+
+//gen搜索控件
+var genKensakuCtrl = new L.Control.Search({
+    position:'topleft',		
+    container:'kensaku',
+    textPlaceholder: '请输入物品名称后按回车搜索3....',
+    textErr: '未搜索到相关内容',
+    layer: genKensaku,
+    initial: false, 
+    zoom: 4, 
+    delayType:40, 
+    collapsed:false, 
+    marker: false})
+//map.addControl(genKensakuCtrl);
+genKensakuCtrl.on('search:locationfound', function(e) {    
+	e.layer.openPopup();
+});   
+
+
+map.addLayer(she);   map.addLayer(shc);   map.addLayer(shf); map.addLayer(shn);   map.addLayer(sho);
+
+
+   
+  //===============================================       
+            function mapShow(x){
+                let s=document.querySelector("#groupShio"),
+                    f=document.querySelector("#groupFu"),
+                    g=document.querySelector("#groupGen"),
+                    sx=document.getElementsByName('shiokaze'),
+                    fx=document.getElementsByName('futago'),
+                    gx=document.getElementsByName('gen'),
+                    sy=document.getElementsByName('shiokazeSQ'),
+                    fy=document.getElementsByName('futagoSQ'),
+                    gy=document.getElementsByName('genSQ');
+
+                    switch(x){
+                     case "1":
+                        shiokaze.addTo(map);
+                        futago.removeFrom(map);
+                        gengetsu.removeFrom(map);
+                        map.addLayer(she);   map.addLayer(shc);   map.addLayer(shf); map.addLayer(shn);   map.addLayer(sho);
+                        fue.removeFrom(map); fuc.removeFrom(map); fuf.removeFrom(map);fuo.removeFrom(map); fun.removeFrom(map);
+                        gee.removeFrom(map); gec.removeFrom(map); gef.removeFrom(map);gen.removeFrom(map); geo.removeFrom(map);
+                        
+                        s.setAttribute('style', 'display:block'); f.setAttribute('style', 'display:none');g.setAttribute('style', 'display:none');
+                        for (u = 0; u < sx.length; u++){sx[u].setAttribute("value", "show") ;sx[u].classList.remove('transprt');sy[u].classList.remove('displayNone')};
+                        map.removeControl(shioKensakuCtrl);map.removeControl(futaKensakuCtrl);map.removeControl(genKensakuCtrl);map.addControl(shioKensakuCtrl)
+
+                         break;
+
+                     case "2":
+                         futago.addTo(map);
+                         shiokaze.removeFrom(map);
+                         gengetsu.removeFrom(map);
+                         map.addLayer(fue);   map.addLayer(fuc);   map.addLayer(fuf);map.addLayer(fun);   map.addLayer(fuo);
+                         gee.removeFrom(map); gec.removeFrom(map); gef.removeFrom(map);gen.removeFrom(map); geo.removeFrom(map);
+                         she.removeFrom(map); shc.removeFrom(map); shf.removeFrom(map);shn.removeFrom(map); sho.removeFrom(map);
+
+
+                         s.setAttribute('style', 'display:none'); f.setAttribute('style', 'display:block');g.setAttribute('style', 'display:none');
+                         for (u = 0; u < fx.length; u++){fx[u].setAttribute("value", "show") ;fx[u].classList.remove('transprt');fy[u].classList.remove('displayNone')};
+
+                         map.removeControl(shioKensakuCtrl);map.removeControl(futaKensakuCtrl);map.removeControl(genKensakuCtrl);map.addControl(futaKensakuCtrl);
+
+
+
+                         break;
+
+                     case "3":
+                         shiokaze.removeFrom(map);
+                         futago.removeFrom(map);
+                         gengetsu.addTo(map);
+                         map.addLayer(gee);   map.addLayer(gec);   map.addLayer(gef);map.addLayer(gen);   map.addLayer(geo);
+                         she.removeFrom(map); shc.removeFrom(map); shf.removeFrom(map);shn.removeFrom(map); sho.removeFrom(map);
+                         fue.removeFrom(map); fuc.removeFrom(map); fuf.removeFrom(map);fuo.removeFrom(map); fun.removeFrom(map);
+
+                         
+                         s.setAttribute('style', 'display:none'); f.setAttribute('style', 'display:none');g.setAttribute('style', 'display:block');
+                         for (u = 0; u < gx.length; u++){gx[u].setAttribute("value", "show") ;gx[u].classList.remove('transprt');gy[u].classList.remove('displayNone')};
+                         map.removeControl(shioKensakuCtrl);map.removeControl(futaKensakuCtrl);map.removeControl(genKensakuCtrl);map.addControl(genKensakuCtrl);
+                         break;
+             }};
+            
+    //mapShow(1);
+
+
+//切换marker显示
+
+function henkanki(z) {
+
+   let      xa=document.getElementById(z);
+            xb=document.getElementById(z+'SQ');
+            x=xa.getAttribute("value");
+            y= eval(z);
+   
+    switch(x){
+    case "hidden": 
+    y.addTo(map);
+    xa.setAttribute("value", "show") ;
+    xb.classList.remove('displayNone');
+    xa.classList.remove('transprt');
+    break;
+
+    case "show":
+    y.removeFrom(map);
+    xa.setAttribute("value", "hidden");
+    xa.classList.add('transprt');
+    xb.classList.add('displayNone');
+    break;
+    }
+}
+
+
+
+
+
+//================移动端搜索
+
+
+var searcherM = new L.Control.Search({
+    position:'topleft',		
+    container:'kensakuM',
+    textPlaceholder: '',
+    textErr: '未搜索到相关内容',
+    layer: markerSearch,
+    initial: false, 
+    zoom: 4, 
+    delayType:40, 
+    collapsed:false, //控制搜索框默认是否折叠
+    marker: false})
+map.addControl(searcherM);
+
+
+
+searcherM.on('search:locationfound', function(e) {    
+	e.layer.openPopup();
+    let jx=document.getElementById("kensakuM");
+
+    jx.style.height="0"; 
+        document.querySelector("#searchtext10").setAttribute('style', 'height: 0px; border: 0px solid #FFAB00');
+        document.querySelector("#kensakuM > div > a.search-button").setAttribute('style', 'height: 0px; border: 0px solid #FFAB00');
+        jx.setAttribute("value", "hidden") ;
+        document.querySelector("#kensakuM > div > ul").setAttribute('style', 'display:none');
+        document.querySelector("#kensakuM > div > a.search-cancel").setAttribute('style', 'display:none');
+       
+        let jy=document.getElementById("menuM");
+        jz=jy.getAttribute("value");
+    //console.log(jz);
+        if(jz=="show"){
+        jy.style.height="0"; 
+        jy.setAttribute("value", "hidden") ;
+}
+});  
+
+
+if (window.screen.width<330){
+    alert('屏幕分辨率宽度过低，会影响部分显示效果')
+};
+
+
+//获取坐标
+function showCood(i) {
+    document.getElementById("coodL").innerHTML = "当前坐标---" + i.latlng};
+    map.on('click', showCood);
+
+function sliderX() {
+    document.getElementById('side').classList.toggle("panelHidden"),
+    document.getElementById('slider').classList.toggle("slider")
+}
+ 
+function sliderM() {
+    document.getElementById('side').classList.toggle("panelHiddenM"),
+    document.getElementById('sliderM').classList.toggle("sliderMx")
+}
+ 
